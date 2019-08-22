c2b1971f97ef5cca56decbb6119d26d1285eee9e
acpucio <acpucio@gmail.com>
2019-08-22 16:13:23 -0400

    First Node.js server

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var instructionsNewVisitor = function(req, res) {
+    res.writeHead(200);
+    res.end('Hi everybody!');
+    }
+    
+var server = http.createServer(instructionsNewVisitor);
