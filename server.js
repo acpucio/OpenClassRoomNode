a44eaf92b6f9d3b34263ae8ad49a9b5ecfd81237
acpucio <acpucio@gmail.com>
2019-08-22 19:42:28 -0400

    More HTML

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var http = require('http');
 
 var instructionsNewVisitor = function(req, res) {
-    res.writeHead(200);
-    res.end('Hi everybody!');
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write('<!DOCTYPE html>'+
+    '<html>'+
+    ' <head>'+
+    ' <meta charset="utf-8" />'+
+    ' <title>My Node.js page! By Golly</title>'+
+    ' </head>'+ 
+    ' <body>'+
+    ' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
+    ' </body>'+
+    '</html>');
+    res.end();
     }
     
 var server = http.createServer(instructionsNewVisitor);
 
 server.listen(8080);
 
-// Comment to test with
 
-// more commentary
